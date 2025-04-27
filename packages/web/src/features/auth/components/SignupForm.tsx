63cc912b4b638c9fedda19f65b7fad2a6ecfce3b
Yuta Uchida <yuchida4dev@gmail.com>
2025-04-27 09:15:48 +0900

    feat: implement SignupForm component with custom useAuthHook for user registration

---
+++ packages/web/src/features/auth/components/SignupForm.tsx
@@ -0,0 +1,76 @@
+import { HeroFormContainer } from '@/elements/forms/HeroFormContainer';
+import { LabeledTextField } from '@/elements/forms/LabeledTextField';
+import { useAuthHook } from '@/features/auth/hooks/useAuthHook';
+
+const DEFAULT_VALUES = {
+  username: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+export function SignupForm() {
+  const { Field, Subscribe, handleSubmit } = useAuthHook({
+    defaultValues: DEFAULT_VALUES,
+    onSubmit: (data) => {
+      console.log(data);
+    },
+  });
+
+  return (
+    <HeroFormContainer
+      title="Sign up"
+      // TODO: Add image src
+      imageSrc="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex flex-col gap-2">
+        <Field name="username">
+          {(field) => (
+            <LabeledTextField
+              label="Username"
+              value={field.state.value}
+              onChange={field.handleChange}
+            />
+          )}
+        </Field>
+        <Field name="email">
+          {(field) => (
+            <LabeledTextField
+              label="Email"
+              value={field.state.value}
+              onChange={field.handleChange}
+            />
+          )}
+        </Field>
+        <Field name="password">
+          {(field) => (
+            <LabeledTextField
+              label="Password"
+              value={field.state.value}
+              type="password"
+              onChange={field.handleChange}
+            />
+          )}
+        </Field>
+        <Field name="confirmPassword">
+          {(field) => (
+            <LabeledTextField
+              label="Confirm Password"
+              value={field.state.value}
+              type="password"
+              onChange={field.handleChange}
+            />
+          )}
+        </Field>
+      </div>
+      <Subscribe selector={(state) => state.isSubmitting}>
+        {(isSubmitting) => (
+          <button className="btn btn-primary" type="submit" disabled={isSubmitting}>
+            Sign up
+          </button>
+        )}
+      </Subscribe>
+    </HeroFormContainer>
+  );
+}
